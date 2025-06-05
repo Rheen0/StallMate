f5207db7d7f08ab78d4f130e186121c0e615b0d1
Jarell Vincent Fababier <jvfababier@gmail.com>
2025-06-05 16:42:19 +0800

    Deleted testing file

--- src/main/java/com/puplagoon/pos/service/Testing.java
+++
@@ -1,6 +0,0 @@
-package src.main.java.com.puplagoon.pos.service;
-
-public class Testing {
-    // Testing github Thermal-Printer integration push and commit
-    // Checking how to add thermal printer branch in github
-}
